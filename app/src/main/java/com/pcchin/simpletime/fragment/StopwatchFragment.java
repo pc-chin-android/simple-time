587d62d06b21a2d0295007f4412e61f35a399ec7
pc.chin <chin.pengcheng@gmail.com>
2019-03-23 22:04:58 +0800

    Started on stopwatch

--- app/src/main/java/com/pcchin/simpletime/fragment/StopwatchFragment.java
+++ app/src/main/java/com/pcchin/simpletime/fragment/StopwatchFragment.java
@@ -1,5 +1,7 @@
 package com.pcchin.simpletime.fragment;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
@@ -12,10 +14,52 @@ import com.pcchin.simpletime.thread.StopwatchThread;
 
 public class StopwatchFragment extends Fragment {
     private StopwatchThread stopwatchThread;
+    private SharedPreferences sharedPref;
+    private boolean isRunning;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        if (getContext() != null) {
+            sharedPref = getContext()
+                    .getSharedPreferences("com.pcchin.simpletime", Context.MODE_PRIVATE);
+        }
         return inflater.inflate(R.layout.fragment_stopwatch, container, false);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        // Get values
+        isRunning = sharedPref.getBoolean("stopwatchRunning", false);
+
+        // Start if stopwatch is running
+        if (isRunning) {
+            int elapsedTime = sharedPref.getInt("stopwatchTime", 0);
+            long pausedTime = sharedPref.getLong("stopwatchPausedTime", 0);
+            stopwatchThread = new StopwatchThread(elapsedTime +
+                    (System.nanoTime() - pausedTime));
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    private void onStopwatchStart() {
+
+    }
+
+    private void onStopwatchPause() {
+
+    }
+
+    private void onStopwatchStop() {
+
+    }
+
+    private void onStopwatchLap() {
+
     }
 }
--- app/src/main/java/com/pcchin/simpletime/thread/StopwatchThread.java
+++ app/src/main/java/com/pcchin/simpletime/thread/StopwatchThread.java
@@ -1,4 +1,19 @@
 package com.pcchin.simpletime.thread;
 
 public class StopwatchThread extends Thread {
+    private long elapsedTime;
+    private long startTime;
+
+    public StopwatchThread(long elapsedTime) {
+        this.elapsedTime = elapsedTime;
+    }
+
+    @Override
+    public void run() {
+
+    }
+
+    public long getElapsedTime(int elapsedTime) {
+        return this.elapsedTime;
+    }
 }
